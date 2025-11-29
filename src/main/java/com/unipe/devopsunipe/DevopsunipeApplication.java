cf925a751ade145d1b92e1cdc858b28551f59c63
caiiohenryk <caiohc.dev@gmail.com>
2025-11-29 09:05:17 -0300

    feat: base project

---
+++ src/main/java/com/unipe/devopsunipe/DevopsunipeApplication.java
@@ -0,0 +1,13 @@
+package com.unipe.devopsunipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsunipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsunipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unipe/devopsunipe/DevopsunipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipe.devopsunipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsunipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
